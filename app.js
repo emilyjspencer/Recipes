bf00206f7ba84622c468200daee9f0a91f32e0a2
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-03 00:54:52 +0100

    Adds a Public folder with stylesheets subfolder. Adds a CSS file and makes this accessible. Imports Dancing Script

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('public'));
 
 const mongoose = require("mongoose");
 
