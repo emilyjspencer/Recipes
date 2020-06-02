ab2d7a9e4937d53cce800922ef13163436f7b945
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 22:31:23 +0100

    Adds server file. Sets the templating engine to ejs. Creates a views folder and adds the basic shell for the recipes view. Imports Dancing Script font and fontawesome

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+
+app.listen(3000, function() {
+  console.log("Listening for requests on port 3000")
+})
+
