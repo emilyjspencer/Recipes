e857731b0057c87bc7959ab55dda25e522168cfd
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 22:40:11 +0100

    Data can be sent from the input box to the server

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 
 
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('recipes.ejs')
 })
+
+app.post('/',(req, res) => {
+  console.log(req.body);
+});
 
 app.listen(3000, function() {
   console.log("Listening for requests on port 3000")
