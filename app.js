3f9415380629b0adfb0e3f75d7ddff6b5de207f2
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 22:48:55 +0100

    Adds Mongoose for database connection

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+const mongoose = require("mongoose");
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.CONNECTION, { useNewUrlParser: true }, () => {
+
+console.log("Connection to the database has been established");
+
+app.listen(3000, function() {
+  console.log("Listening for requests on port 3000")
+});
 
 app.get('/', (req, res) => {
   res.render('recipes.ejs')
@@ -14,7 +26,7 @@ app.post('/',(req, res) => {
   console.log(req.body);
 });
 
-app.listen(3000, function() {
-  console.log("Listening for requests on port 3000")
-})
+});
+
+
 
