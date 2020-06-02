0e023f6a81e07e7f7f9429de7fee5941253d6241
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-02 22:34:20 +0100

    Adds home route. Renders the recipes view

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.set("view engine", "ejs");
 
 
+app.get('/', (req, res) => {
+  res.render('recipes.ejs')
+})
+
 app.listen(3000, function() {
   console.log("Listening for requests on port 3000")
 })
